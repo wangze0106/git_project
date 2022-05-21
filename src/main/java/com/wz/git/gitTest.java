a479ac81d60014b38cc5297a9923b794e3443a95
wz <1627612716@qq.com>
2022-05-21 19:45:04 +0800

    hot-fix second commit

--- src/main/java/com/wz/git/gitTest.java
+++ src/main/java/com/wz/git/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
         System.out.println("hello git v2");
         System.out.println("hello git v3");
         System.out.println("hello git hot-fix v1");
+        System.out.println("hello git hot-fix v2");
     }
 }
